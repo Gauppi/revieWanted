31e3e397ac17aed808683db70534c807cb866173
Gauppi <49677135+Gauppi@users.noreply.github.com>
2022-10-31 13:31:09 +0100

    d3 V4 force directed graph
    Template für ein Netzwerk eingefügt.
    Befüllt mit Beispieldaten: "miserables.json" -> besteht aus Knoten und Links

--- src/script.js
+++ src/script.js
@@ -43,7 +43,7 @@ async function search() {
         let data = combineData(iCiteData, iCiteData2);
         console.log("JSON created:",data);
         console.log("directed Links: ", directedlinks);
-        //showGraph(data);
+        showGraph(data);
     } catch(e) {
         console.log(e)
     }
@@ -200,13 +200,13 @@ async function getICiteData2(idlist) {
 }
 
 function showGraph(data) {
-    let svg = d3.select("svg"),
+    var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
 
-    let color = d3.scaleOrdinal(d3.schemeCategory20);
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-    let simulation = d3.forceSimulation()
+    var simulation = d3.forceSimulation()
         .force("link", d3.forceLink().id(function(d) { return d.id; }))
         .force("charge", d3.forceManyBody())
         .force("center", d3.forceCenter(width / 2, height / 2));
@@ -214,32 +214,32 @@ function showGraph(data) {
     d3.json("miserables.json", function(error, graph) {
     if (error) throw error;
 
-    let link = svg.append("g")
+    var link = svg.append("g")
         .attr("class", "links")
         .selectAll("line")
         .data(graph.links)
         .enter().append("line")
         .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
 
-    let node = svg.append("g")
+    var node = svg.append("g")
         .attr("class", "nodes")
         .selectAll("g")
         .data(graph.nodes)
         .enter().append("g")
 
-    let circles = node.append("circle")
+    var circles = node.append("circle")
         .attr("r", 5)
         .attr("fill", function(d) { return color(d.group); });
 
     // Create a drag handler and append it to the node object instead
-    let drag_handler = d3.drag()
+    var drag_handler = d3.drag()
         .on("start", dragstarted)
         .on("drag", dragged)
         .on("end", dragended);
 
     drag_handler(node);
     
-    let lables = node.append("text")
+    var lables = node.append("text")
         .text(function(d) {
             return d.id;
         })
@@ -256,37 +256,37 @@ function showGraph(data) {
     simulation.force("link")
         .links(graph.links);
 
+    function ticked() {
+        link
+            .attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
     
-        function ticked() {
-            link
-                .attr("x1", function(d) { return d.source.x; })
-                .attr("y1", function(d) { return d.source.y; })
-                .attr("x2", function(d) { return d.target.x; })
-                .attr("y2", function(d) { return d.target.y; });
-            node
-                .attr("transform", function(d) {
+        node
+            .attr("transform", function(d) {
                 return "translate(" + d.x + "," + d.y + ")";
-                })
+            })
         }
-        function dragstarted(d) {
-            if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-            d.fx = d.x;
-            d.fy = d.y;
-        }
-        
-            function dragged(d) {
-            d.fx = d3.event.x;
-            d.fy = d3.event.y;
-        }
-        
-            function dragended(d) {
-            if (!d3.event.active) simulation.alphaTarget(0);
-            d.fx = null;
-            d.fy = null;
-        } 
     });
-       
 }
+function dragstarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+}
+
+function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+}
+
+function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+}
+    
 
 
 
